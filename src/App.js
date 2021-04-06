562c5548d2a66830930044e14c8bcdf9bc5af2d4
chandana <jampala.c@gmail.com>
2021-04-06 18:05:05 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Header/>
+      {/* home */}
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+function Header() {
+    return (
+        <div class='header'>
+            <img className='header__logo' src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+            <div className='header__search'>
+                <input className='header__searchInput' type='text' />
+                <SearchIcon className='header__searchIcon' />
+            </div>
+            <div className='header__nav'>
+                <div className='header__menu'>
+                    <span className='header__optionLineOne'>
+                        Hello
+                    </span>
+                    <span className='header__optionLineTwo'>
+                        Sign in
+                    </span>
+                </div>
+                <div className='header__menu'>
+                    <span className='header__optionLineOne'>
+                        Returns
+                    </span>
+                    <span className='header__optionLineTwo'>
+                        & Orders
+                    </span>
+                </div>
+                <div className='header__menu'>
+                    <span className='header__optionLineOne'>
+                        Your
+                    </span>
+                    <span className='header__optionLineTwo'>
+                        Prime
+                    </span>
+                </div>
+                <div className='header__navcart'>
+                    <span className='header__cart'>
+                    <span className='header__count '>0</span>
+                    <ShoppingCartIcon className='header__cartIcon'/>
+                    </span>
+                    <span className='header__cartOption'>Cart</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
